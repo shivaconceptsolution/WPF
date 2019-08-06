6f67860df07d635a8ed9f9746ccfee6aa8b96edf
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-08-06 12:03:35 +0530

    Add files via upload

---
+++ Window3.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (Chk1.IsChecked == true)
+                sb.Append(" C ");
+            if (Chk2.IsChecked == true)
+                sb.Append(" CPP ");
+
+            lblres.Content = sb.ToString();
+        }
+    }
+}
---
+++ Window4.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for Window4.xaml
+    /// </summary>
+    public partial class Window4 : Window
+    {
+        public Window4()
+        {
+            InitializeComponent();
+            c1.Items.Add("C");
+            c1.Items.Add("CPP");
+            c1.Items.Add("DS");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lblres.Content = c1.SelectedItem.ToString();
+        }
+    }
+}
